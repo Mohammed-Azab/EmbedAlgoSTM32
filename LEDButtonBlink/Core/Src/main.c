3180e8f37c561d940746590dd205b63519e13c3b
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-03-27 12:13:55 +0100

    Fixed Include Libaray issues

---
+++ LEDButtonBlink/Core/Src/main.c
@@ -0,0 +1,102 @@
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdbool.h>
+
+void configureIO();
+void enableClk();
+void light(char i);
+
+
+
+int main(void){
+
+	enableClk();
+	configureIO();
+	_Bool pressed = false;
+
+	char c = 0;
+
+
+	while (1) {
+
+	  if (GPIOB -> IDR & (1 << 10)){
+		  HAL_Delay(50);
+		  if (GPIOB -> IDR & 1 << 10)
+			  pressed = true;
+	  }
+
+
+	  if (c<5 && pressed){
+		  	  pressed = false;
+		  	  light(++c);
+	  }
+
+	  else if (c++ == 5){
+		  	  light(2);
+	  }
+
+
+   	  }
+
+
+}
+
+
+
+void configureIO(){
+
+	GPIOA -> CRL = 0x44444422; //output 2 MHz A0,A1
+	GPIOB -> CRH = 0x44444844; //input pull-down/up. B10
+}
+
+void enableClk(){
+	RCC -> APB2ENR |= RCC_APB2ENR_IOPAEN;
+	RCC -> APB2ENR |= RCC_APB2ENR_IOPBEN;
+}
+
+
+void light (char i){
+
+	switch (i){
+			case 1: GPIOA -> ODR |= 1 << 0;   break; //A0
+			case 2: GPIOA -> ODR |= 1 << 1;   break; //A1
+			default: break;
+		}
+
+}
+
+
+
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
