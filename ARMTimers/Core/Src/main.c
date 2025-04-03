e6487bf768c785090293d229a53676cc586db8ce
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-04-03 20:52:59 +0200

    Toggling Led using 2 buttons are now completed

--- ARMTimers/Core/Src/main.c
+++ ARMTimers/Core/Src/main.c
@@ -1,80 +1,93 @@
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include <math.h>
 
 
-
-void enableClk();
 void configureIO();
+void enableClk();
 void delay(int t);
 
 
 
-int main(void) {
 
+int main(void){
+	HAL_Init();
 	enableClk();
 	configureIO();
 
-  while (1) {
-
-	  if (GPIOB ->IDR & (1<<10)){ //Active High
-
-		  delay(60);
-
-		  if (GPIOB ->IDR & (1<<10)){
-
-			  while (-1){
-			  			  GPIOA -> ODR ^= (1 << 1);
-			  			  delay(100);
-			  		  }
-
-		  }
 
 
-	  }
-	  else if (!(GPIOB ->IDR & (1<<11))){ //Active low
-
-		  delay(60);
-
-		  if (!(GPIOB ->IDR & (1<<11))){
-
-			  while (-1){
-		  			  GPIOA -> ODR ^= (1 << 1);
-		  			  delay(500);
-		  		  }
-		  }
-
-	  }
+	while (1) {
 
 
-  }
+		if (GPIOB -> IDR & (1 << 10)){ //Active High
 
+			while (-1){
+				GPIOA -> ODR ^= 1 << 1;
+				delay(100);
+			}
+
+		}
+
+		else if (!(GPIOB ->IDR & (1<<11))){ //Active low
+
+			while (-1){
+				GPIOA -> ODR ^= 1 << 1;
+				delay(500);
+			}
+
+		}
+
+
+
+	}
+}
+
+
+
+void configureIO(){
+
+	GPIOA -> CRL = 0x44444424; //output 2 MHz A1
+	//GPIOA -> CRL |= 0x444444A4; //A1 Alternating Function output 2MHz
+	GPIOB -> CRH = 0x44448844; //input pull-down/up. B10, B11
+	GPIOB->ODR |= (1 << 11); // B11 Pull UP
+	GPIOB->ODR &= ~(1 << 10); // B10 pull down
 }
 
 void enableClk(){
-
 	RCC -> APB2ENR |= RCC_APB2ENR_IOPAEN;
 	RCC -> APB2ENR |= RCC_APB2ENR_IOPBEN;
 	RCC -> APB1ENR |= RCC_APB1ENR_TIM2EN;
-
 }
-void configureIO(){
 
-	GPIOB -> CRH |= 0x44448844; //B10 & B10 input
-	GPIOA -> CRL |= 0x444444A4; //A1 Alternating Function output 2MHz
-	GPIOB -> ODR |= (1<<11); //B11 Pull Up // B10 Pull down by default
-
-}
 void delay(int t){
 	TIM2 ->PSC = 8000-1;
-	int ARRValP1 = ((t/1000) * 8 * pow(10,6)) / 8000 ;
-	TIM2 ->ARR = ARRValP1-1;
+	//int ARRValP1 = ((t) * 8 * pow(10,3)) / 8000 ;
+	TIM2 ->ARR = t-1;
+	TIM2 ->CR1 |= TIM_CR1_CEN; //Enable the counter
+	while(!(TIM2->SR & TIM_SR_UIF)); //Waiting until finishing counting
+	TIM2->SR &= ~TIM_SR_UIF; //reset the flag
 
 }
 
 
 
 
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
 
 #ifdef  USE_FULL_ASSERT
 /**
