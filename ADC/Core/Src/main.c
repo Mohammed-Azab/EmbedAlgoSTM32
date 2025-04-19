7c0b15f2cd7046473d37c4318e5505121e62a360
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-04-19 22:39:08 +0200

    Tested Hardware output

--- ADC/Core/Src/main.c
+++ ADC/Core/Src/main.c
@@ -8,7 +8,6 @@ void enableClk();
 
 
 int main(void){
-	HAL_Init();
 	enableClk();
 	configureIO();
 
@@ -16,7 +15,16 @@ int main(void){
 
 	while (1) {
 
-		GPIOA -> ODR |= (1 << 4);
+		GPIOB -> ODR |= (1 << 13);
+		GPIOB -> ODR |= (1 << 9);
+		GPIOB -> ODR |= (1 << 8);
+		GPIOB -> ODR |= (1 << 6);
+		GPIOB -> ODR |= (1 << 15);
+		GPIOB -> ODR |= (1 << 12);
+		GPIOB -> ODR |= (1 << 0);
+		GPIOB -> ODR |= (1 << 7);
+		GPIOB -> ODR |= (1 << 10);
+		GPIOB -> ODR |= (1 << 11);
 
 
 	}
@@ -27,20 +35,16 @@ int main(void){
 
 void configureIO(){
 
-	GPIOA -> CRL |= 0x44444448; //A0 input
-
-
 	/*
-	 * Output signal Sequence B9 B7 B4 A15 A9 B15 B13 A4 A6 B11
+	 * Output signal Sequence B9 B8 B7 B6 B15 B13 B12 B0 B10 B11
 	 *
-	 * A => A4 A6 A9 A15
-	 * B => B4 B7 B9 B11 B13 B15
+	 * B => B0 B6 B7 B8 B9 B10 B11 B12 B13 B15
+	 *
 	 * */
 
-	GPIOA -> CRL |= 0x42424444; // A4 A6 output 8MHz
-	GPIOA -> CRH |= 0x24444424; // A9 A15 output 8MHz
-	GPIOB -> CRL |= 0x24424444; // B4 B7 output 8MHz
-	GPIOB -> CRH |= 0x24242424; // B9 B11 B13 B15 output 8MHz
+	GPIOA -> CRL = 0x44444448;  // A0 input
+	GPIOB -> CRL = 0x22444442;  // B0 B6 B7 output 8MHz
+	GPIOB -> CRH = 0x24222222;  // B8 B9 B10 B11 B12 B13 B15 output 8MHz
 
 }
 
