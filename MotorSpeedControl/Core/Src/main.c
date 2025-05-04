b67ed5441e90c0ba7ad560bae276542e1b4d82e7
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-05-04 19:58:13 +0200

    Configured 2 buttons

--- MotorSpeedControl/Core/Src/main.c
+++ MotorSpeedControl/Core/Src/main.c
@@ -14,7 +14,7 @@ int main(void){
 	configureIO();
 
 
-	initADC();
+	//initADC();
 
 	uint16_t prev;
 	uint8_t flag = 1;
@@ -23,36 +23,6 @@ int main(void){
 	while (1) {
 
 
-		ADC1->CR2 |= ADC_CR2_SWSTART; // start conversion
-		while (!(ADC1->SR & ADC_SR_EOC));     // Wait for conversion complete
-		uint16_t ADCVal = ADC1->DR & 0x3FF;  // Read result (10-bit mask)
-
-
-		/*
-		 *  Why & 0x3FF?
-		 *  0x3FF in hexadecimal = 1023 in decimal = 0b0000001111111111 in binary (10 bits set to 1).
-		 *  This mask ensures you only keep the lowest 10 bits and discard any upper garbage bits that might accidentally exist in ADC1->DR.
-		 *  It's a way to guarantee you're reading a clean 10-bit result
-		 *
-		 * */
-
-
-		if (flag){
-			prev = ADCVal;
-			flag = 0;
-		}
-		else if (prev == ADCVal){
-			continue;
-		}
-
-		GPIOB -> ODR &= 0; //reset all
-
-		uint8_t num_leds_on = ADCVal / 102;  // 1023 / 10 ≈ 102.3
-
-		for (uint8_t i = 0 ; i< num_leds_on; i++){
-
-			turnOn(i);
-		}
 
 
 
@@ -68,15 +38,14 @@ int main(void){
 void configureIO(){
 
 	/*
-	 * Output signal Sequence B9 B8 B7 B6 B15 B13 B12 B0 B10 B11
 	 *
-	 * B => B0 B6 B7 B8 B9 B10 B11 B12 B13 B15
+	 * LEDS => B10 A7 output
+	 * Buttons => B13 B14 Input pull down (Active high)
 	 *
 	 * */
 
-	GPIOA -> CRL = 0x44444440;  // A0 input Analog
-	GPIOB -> CRL = 0x22444442;  // B0 B6 B7 output 8MHz
-	GPIOB -> CRH = 0x24222222;  // B8 B9 B10 B11 B12 B13 B15 output 8MHz
+	GPIOA -> CRL = 0x24444444;
+	GPIOB -> CRH = 0x48844444;
 
 }
 
@@ -114,38 +83,16 @@ void initADC(){
 
 void turnOn(uint8_t i){
 
-	// Output signal Sequence B9 B8 B7 B6 B15 B13 B12 B0 B10 B11
 
 	switch (i){
 
-		case 0: GPIOB ->ODR |= (1 << 9); return;
-		case 1: GPIOB ->ODR |= (1 << 8); return;
-		case 2: GPIOB ->ODR |= (1 << 7); return;
-		case 3: GPIOB ->ODR |= (1 << 6); return;
-		case 4: GPIOB ->ODR |= (1 << 15); return;
-		case 5: GPIOB ->ODR |= (1 << 13); return;
-		case 6: GPIOB ->ODR |= (1 << 12); return;
-		case 7: GPIOB ->ODR |= (1 << 0); return;
-		case 8: GPIOB ->ODR |= (1 << 10); return;
-		case 9: GPIOB ->ODR |= (1 << 11); return;
+		case 0: GPIOA ->ODR |= (1 << 7); return;
+		case 1: GPIOB ->ODR |= (1 << 10); return;
 
 		default: break;
 	}
 }
 
-
-/*
-GPIOB -> ODR |= (1 << 13);
-GPIOB -> ODR |= (1 << 9);
-GPIOB -> ODR |= (1 << 8);
-GPIOB -> ODR |= (1 << 6);
-GPIOB -> ODR |= (1 << 15);
-GPIOB -> ODR |= (1 << 12);
-GPIOB -> ODR |= (1 << 0);
-GPIOB -> ODR |= (1 << 7);
-GPIOB -> ODR |= (1 << 10);
-GPIOB -> ODR |= (1 << 11);
-*/
 
 
 
