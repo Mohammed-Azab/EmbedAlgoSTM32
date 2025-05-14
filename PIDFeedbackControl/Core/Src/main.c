00eee3fc0eaaa8195a22bc28b8c7b2401a68578b
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-05-14 22:53:56 +0200

    Added ADC Sample Time

--- PIDFeedbackControl/Core/Src/main.c
+++ PIDFeedbackControl/Core/Src/main.c
@@ -127,6 +127,7 @@ void enableClk(){
 	RCC -> APB2ENR |= RCC_APB2ENR_IOPAEN;
 	RCC -> APB2ENR |= RCC_APB2ENR_IOPBEN;
 	RCC -> APB1ENR |= 0b11; //enable TIM2 & TIM3
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 	RCC -> APB2ENR |= (1 << 9) | (1 << 10); // enable CLK for ADC1
 }
 
@@ -136,6 +137,7 @@ void initADC1(){
 		ADC1->SQR1 = 0x00000000;  // Reset SQR1 (sequence length = 1 by default)
 		ADC1->SQR3 = 0; // Channel 0 first in the sequence
 		ADC1 -> SMPR2 &= 0; // reseting the sample time
+		ADC1->SMPR2 |= (0b010 << 0); // Channel 0 7.5 cycles
 
 		ADC1->CR2 |= ADC_CR2_ADON; // ADC on
 		for (volatile int i = 0; i < 10000; i++);  // Short delay
@@ -160,6 +162,7 @@ void initADC2(){
 		ADC2->SQR1 = 0x00000000;  // Reset SQR1 (sequence length = 1 by default)
 		ADC2->SQR3 = 2; // Channel 2
 		ADC2 -> SMPR2 &= 0; // reseting the sample time
+		ADC2->SMPR2 |= (0b010 << 6); // Channel 2 7.5 cycles
 
 		ADC2->CR2 |= ADC_CR2_ADON; // ADC on
 		for (volatile int i = 0; i < 10000; i++);  // Short delay
