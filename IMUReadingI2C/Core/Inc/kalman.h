76676052b149647ac1497c3efb2baa30e7619216
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-05-22 16:39:09 +0200

    Implemented integral for the yaw angle

--- IMUReadingI2C/Core/Inc/kalman.h
+++ IMUReadingI2C/Core/Inc/kalman.h
@@ -4,18 +4,14 @@
 #define KALMAN_H
 
 typedef struct {
-    float Q_angle;  // Process noise variance for the accelerometer
-    float Q_bias;   // Process noise variance for the gyro bias
-    float R_measure; // Measurement noise variance
+    float angle;
+    float bias;
+    float rate;
 
-    float angle;    // The angle calculated by the Kalman filter
-    float bias;     // The gyro bias calculated by the Kalman filter
-    float rate;     // Unbiased rate
-
-    float P[2][2];  // Error covariance matrix
+    float P[2][2];
 } Kalman_t;
 
 void Kalman_Init(Kalman_t *kf);
-float Kalman_GetAngle(Kalman_t *kf, float newAngle, float newRate, float dt);
+float Kalman_getAngle(Kalman_t *kf, float newAngle, float newRate, float dt);
 
-#endif // KALMAN_H
+#endif
