4c104b786be105440b1f3a4d7c4a740f7954c972
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-05-26 20:42:05 +0200

    Implemented 2 task for toggling at diff freq.

--- FreeRTOS/Core/Src/main.c
+++ FreeRTOS/Core/Src/main.c
@@ -1,17 +1,28 @@
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "stm32f1xx.h"
+#include "queue.h"
+#include "timers.h"
+#include "semphr.h"
+#include "event_groups.h"
 
-void Task1(void* arg);
+
+
+void TaskBlinkLED1(void *arg);
+
+void TaskBlinkLED2(void *arg);
 
 
 int main(){
 
-	xTaskCreate(Task1,"T1",128,NULL, 1 , NULL);
+	RCC ->APB2ENR |= RCC_APB2ENR_IOPBEN;
+	GPIOB -> CRH = 0x44442244;
+
+	xTaskCreate(TaskBlinkLED1,"T1",128,NULL, 1 , NULL);
+	xTaskCreate(TaskBlinkLED2,"T2",128,NULL, 1 , NULL);
 	vTaskStartScheduler();
 
 }
@@ -20,6 +31,20 @@ int main(){
 
 
 
-void Task1(void* arg){
-
+void TaskBlinkLED1(void *arg){
+	while (1){
+		GPIOB ->ODR ^= (1 << 10);
+		vTaskDelay(pdMS_TO_TICKS(50)); // 10Hz -> 1/10 = 100ms -> delay each 50ms
+	}
 }
+
+void TaskBlinkLED2(void *arg){
+	while (1){
+		GPIOB ->ODR ^= (1 << 11);
+		vTaskDelay(pdMS_TO_TICKS(25)); // 20Hz -> 1/20 = 50ms -> delay each 20ms
+	}
+}
+
+
+
+
